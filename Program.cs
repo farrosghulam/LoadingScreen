864585620936e28498af28bbe6c78ae8150d4cc6
farrosghulam <farrosghulam@gmail.com>
2026-09-23 14:03:13 +0700

    adding additional message for a multiple of 3

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@
 for (int step = 1; step <= totalSteps; step++)
 {
     Console.WriteLine($"Loading... Step {step} of {totalSteps}");
+    if (step % 3 == 0)
+    {
+        Console.WriteLine("\nCheckpoint reached!\n");
+    }
 }
 
 Console.WriteLine("\nDungeon is ready. Enter now!");
