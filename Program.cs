85f254ff26831acff37f42d31ece96f5f5543e22
farrosghulam <farrosghulam@gmail.com>
2026-09-23 13:58:24 +0700

    step variable value revision

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿int totalSteps = 15;
 
-for (int step = 0; step <= totalSteps; step++)
+for (int step = 1; step <= totalSteps; step++)
 {
     Console.WriteLine($"Loading... Step {step} of {totalSteps}");
 }
